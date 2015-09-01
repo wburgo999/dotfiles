5b18d4a4654b9fe148326660f780fc6a21456308
Will Burgo <will@cleermob.com>
2015-09-01 13:09:15 -0700

    Get a random fact instead of just a Math fact

--- bash_scripts/numbers.js
+++ bash_scripts/numbers.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 var unirest = require("unirest");
-unirest.get("https://numbersapi.p.mashape.com/6/21/date?fragment=true&json=true")
+unirest.get("https://numbersapi.p.mashape.com/random/trivia?fragment=true&json=true&max=20&min=10")
 .header("X-Mashape-Key", "A7tmiKLaDdmshYKWbAKQPNT97faOp1j1Ni2jsnJhv946VKlfyB")
 .header("Accept", "text/plain")
 .end(function (result) {
+    
     var data = result.body;
-    console.log("\n" + data.text);
-    console.log(data.year);
+    var output = '\n' + data.text + ":\t\t";
+    output += data.number || '';
+    output += data.date || '';
+    output += data.year || '';
+    console.log(output);
+    
 });
